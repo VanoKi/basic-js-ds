a6e4b0e20f02169a186caa48265f6357f416c195
VanoKi <kirsvano@gmail.com>
2025-01-07 01:55:06 +0300

    feat: add min max

--- src/binary-search-tree.js
+++ src/binary-search-tree.js
@@ -71,13 +71,21 @@ class BinarySearchTree {
   }
 
   min() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.root) return;
+    let node = this.root
+    while (node.left) {
+      node = node.left
+    }
+    return;node.data
   }
 
   max() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    if(!this.root) return;
+    let node = this.root
+    while (node.right) {
+      node = node.right
+    }
+    return;node.data
   }
 }
 
